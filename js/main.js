79de78e311e1dbc95407e4a981d022e3891aa3e8
AelitaSerour <114419462+AelitaSerour@users.noreply.github.com>
2023-03-09 20:16:26 +0100

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import {generatePictures} from './data.js';
+import './miniatures.js';
+import './full-size-pictures.js';
+import './users-form.js';
+import './upload.js';
+
+const PICTURES_COUNT = 25;
+
+generatePictures(PICTURES_COUNT);
